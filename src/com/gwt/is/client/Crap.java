f96f309c14521dbe049a3f913080f8cf39236c8c
Patryk <patlaw96@gmail.com>
2015-05-04 16:53:38 +0200

    added bootstrap

--- src/com/gwt/is/client/Crap.java
+++ src/com/gwt/is/client/Crap.java
@@ -12,6 +12,9 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.HasKeyboardSelectionPolicy.KeyboardSelectionPolicy;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
@@ -38,19 +41,56 @@ public class Crap implements EntryPoint {
 	private final MyDataProvider dataProvider = new MyDataProvider();
 	
 	private final TextBox valueField = new TextBox();
-	private final TextBox editField = new TextBox();
 
-	private final Button editButtton = new Button("Update");
+	private final Button editButton = new Button("Update");
 	private final Button addButton = new Button("Create");
 	private final Button removeButton = new Button("Delete");
 
-	private final CellList<String> cellList = new CellList<String>(new TextCell());
+	//private final CellList<String> cellList = new CellList<String>(new TextCell());
+	
+	private final CellTable<String> table = new CellTable<String>();
+	
+	
+	
 	private final CRUDServiceAsync crud = GWT.create(CRUDService.class);
 	 
     
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
+	
+		valueField.addStyleName("form-control");
+		valueField.getElement().setAttribute("placeholder", "Enter news");
+		valueField.getElement().setAttribute("aria-describedby", "basic-addon1");
 		
+		RootPanel.get("inputs").add(valueField);
+		
+		editButton.addStyleName("btn btn-success");
+		editButton.removeStyleName("gwt-Button");
+		editButton.setText("Create");
+		
+		RootPanel.get("buttons").add(editButton);
+		
+		addButton.addStyleName("btn btn-warning");
+		addButton.removeStyleName("gwt-Button");
+		addButton.setText("Update");
+		
+		RootPanel.get("buttons").add(addButton);
+		
+		removeButton.addStyleName("btn btn-danger");
+		removeButton.removeStyleName("gwt-Button");
+		removeButton.setText("Delete");
+		
+		RootPanel.get("buttons").add(removeButton);
+		
+		table.setKeyboardSelectionPolicy(KeyboardSelectionPolicy.ENABLED);
+		
+		TextColumn<String> stringColumn = new TextColumn<String>(){
+			@Override
+			public String getValue(String obj){
+				return obj;
+			}
+		};
+		
+		table.addColumn(stringColumn, "News");
 	}
 }
